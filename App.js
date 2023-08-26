d0e6183908bf386f62333af0bae7ad9a38b35ab4
Jan Wojcik <123651940+Janowoj@users.noreply.github.com>
2023-08-26 23:28:03 +0200

    "event bubbling, event capturing, stop propagation (the event stops bubbling up the DOM tree.), remove.EventListener(), delegation"

--- App.js
+++ App.js
@@ -152,23 +152,113 @@ const child = document.querySelector('.child');
 //  if we move the stopPropagation() method to the child event listener, the output will be:
 //  document capture grandparent capture parent capture child capture child bubble.
 
-grandparent.addEventListener(
-    'click',
-    e => {
-        console.log("grandparent");
-    });
+//  ONCE: if we want to run an event listener only once, we can use the once parameter (THIRD PARAMETER):
 
-parent.addEventListener(
-    'click',
-    e => {
-        console.log("parent");
-    });
+// grandparent.addEventListener(
+//     'click',
+//     e => {
+//         console.log("grandparent");
+//     });
 
-child.addEventListener(
-    'click',
-    e => {
-        console.log("child ");
+// parent.addEventListener(
+//     'click',
+//     e => {
+//         console.log("parent");
+//     },
+//     { once: true });
+
+// child.addEventListener(
+//     'click',
+//     e => {
+//         console.log("child ");
+//     });
+
+// clicking on the parent element will only run the event listener onc, e.g.: clicking on parent,
+// the output will be: grandparent, parent, child. 
+// Clicking on parent again the output will be: grandparent, child.
+
+// .removeEventListener() method: 
+
+// function printHi() {
+//     console.log("hi");
+// }
+
+// parent.addEventListener(
+//     'click',
+//     printHi);
+
+// setTimeout(() => {
+//     parent.removeEventListener('click', printHi);
+// }, 2000);
+
+// the event listener will be removed after 2 seconds.
+
+// DELEGATION: we can use event delegation to add event listeners to elements that are not yet on the page
+
+// const divs = document.querySelectorAll('div');
+
+// console.log(divs);
+
+// divs.forEach(div => {
+//     div.addEventListener('click', () => {
+//         console.log("hi");
+//     })
+// });
+
+// document.body.addEventListener('click', e => {
+//     if (e.target.matches('div')) {
+//         console.log("hi");
+//     };
+// });
+
+// const newDiv = document.createElement('div');
+// newDiv.style.width = '200px';
+// newDiv.style.height = '200px';
+// newDiv.style.backgroundColor = 'purple';
+// document.body.append(newDiv);
+
+//  the new div will not have the event listener attached to it, 
+// because it was created after the event listener was added to the divs.
+
+// newDiv.addEventListener('click', () => {
+//     console.log("new div");
+// });
+// document.body.append(newDiv);
+// output: new div
+
+// instead of this we can use event delegation:
+
+document.body.addEventListener('click', 'div', e => {
+    console.log("hi");
+});
+
+const newDiv = document.createElement('div');
+newDiv.style.width = '200px';
+newDiv.style.height = '200px';
+newDiv.style.backgroundColor = 'purple';
+document.body.append(newDiv);
+
+// when somebody click inside the div, you will see the output: hi, also in the newDiv.
+
+// super useful funkction:
+
+addGlobalEventListener('click', e => {
+    if (e.target.matches('div')) {
+        console.log("hi");
+    };
+});
+
+function addGlobalEventListener(type, selector, callback) {
+    document.addEventListener(type, e => {
+        if (e.target.matches(selector)) callback(e);
     });
+}
+
+
+
+
+
+
 
 
 
